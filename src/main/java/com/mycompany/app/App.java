f8ff9f8b719bf6cb0096e7b129811fa101b47507
Kah Seng Low <kah-seng.low@keysight.com>
2026-04-06 15:24:22 +0800

    Second changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("First local changed. Jenkins is awesome!!!");
+        System.out.println("Second local changed. Jenkins is awesome!!!");
     }
 
     public String getMessage() {
