09054433b9b496827a396f90f7aa7db8ac61c09b
Kah Seng Low <kah-seng.low@keysight.com>
2026-04-06 16:03:53 +0800

    Applied Changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Second local changed. Jenkins is awesome!!!");
+        System.out.println("Applied Changes. Jenkins is awesome!!!");
     }
 
     public String getMessage() {
